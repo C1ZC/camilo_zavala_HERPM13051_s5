7f1aef9d34d7e834dbb1b2c0b6f8c71837296b87
C1ZC <camilo.zavala.c@gmail.com>
2024-06-27 11:20:46 -0400

    cambios

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -4,3 +4,9 @@ echo "Setting up Java 17"
 export JAVA_HOME=$(/usr/libexec/java_home -v 17)
 export PATH=$JAVA_HOME/bin:$PATH
 java -version
+
+# Example: Change bundle name of an iOS app for non-production
+if [ "$APPCENTER_BRANCH" != "main" ];
+then
+    plutil -replace CFBundleDisplayName -string "\$(PRODUCT_NAME) Beta" $APPCENTER_SOURCE_DIRECTORY/MyApp/Info.plist
+fi
